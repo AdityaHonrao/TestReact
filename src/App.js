d5cbfb9ce66a7ab8d8b1854f4ad3230b22bcefbb
Aditya Honrao <aditya0honrao@gmail.com>
2022-09-08 11:10:27 +0530

    Added dynamic tables for students and colleges. CSS has yet to be added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Table from "./table/table";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Litmus Test Student Database</h1>
-      </div>
+        <div className="App">
+          <div className="App-header">
+            <h1>Litmus Test Student Database</h1>
+          </div>
+        <div className="App-body">
+            <div className="Table-row">
+                <Table contents={"Students"}/>
+                <Table contents={"Colleges"}/>
+            </div>
+        </div>
     </div>
   );
 }
